5532bb45bf3c80c3683daada1ea3625ef5e4bfc6
Herald Park <herald1999@gmail.com>
2025-02-15 12:25:59 -0500

    Add compression feature

--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -1,32 +1,49 @@
-import { useState } from 'react'
-import useShowToast from './useShowToast'
+import { useState } from "react";
+import useShowToast from "./useShowToast";
+import imageCompression from "browser-image-compression";
 
 const usePreviewImg = () => {
-    const [selectedFile, setSelectedFile] = useState(null)
-    const showToast = useShowToast()
+  const [selectedFile, setSelectedFile] = useState(null);
+  const showToast = useShowToast();
 
-    const maxFileSizeInBytes = 2 * 1024 * 1024
-    const handleImageChange = (e) => {
-        const file = e.target.files[0]
-        if(file && file.type.startsWith("image/")){
-            if(file.size > maxFileSizeInBytes){
-                showToast("Error", "File size must be less than 2MB", "error")
-                setSelectedFile(null)
-                return
-            }
-            const reader = new FileReader()
+  // const maxFileSizeInBytes = 2 * 1024 * 1024
+  const compressionOptions = {
+    maxSizeMB: 0.2,
+    maxWidthOrHeight: 800,
+    useWebWorker: true,
+  };
 
-            reader.onloadend = () => {
-                setSelectedFile(reader.result)
-            }
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
 
-            reader.readAsDataURL(file)
-        } else {
-            showToast("Error", "Please select an image file", "error")
-            setSelectedFile(null)
-        }
+    if (file && file.type.startsWith("image/")) {
+      // if(file.size > maxFileSizeInBytes){
+      //     showToast("Error", "File size must be less than 2MB", "error")
+      //     setSelectedFile(null)
+      //     return
+      // }
+
+      try {
+        // Compress Image
+        const compressedFile = await imageCompression(file, compressionOptions);
+
+        // Convert compressed image to base64 for previewing
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setSelectedFile(reader.result);
+        };
+
+        reader.readAsDataURL(compressedFile);
+        console.log("Compression successful");
+      } catch {
+        showToast("Error", "Image compression failed", "error");
+        console.error("Compression error:", error);
+      }
+    } else {
+      showToast("Error", "Please select an image file", "error");
+      setSelectedFile(null);
     }
-    return { selectedFile, handleImageChange, setSelectedFile }
-  
-}
-export default usePreviewImg
+  };
+  return { selectedFile, handleImageChange, setSelectedFile };
+};
+export default usePreviewImg;
